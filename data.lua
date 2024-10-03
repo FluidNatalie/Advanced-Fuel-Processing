e9e41d2e2370c1736aa725a48ee77ef56c1fc77a
FluidNatalie <nateandgeorge@gmail.com>
2024-10-03 15:18:16 +0200

    Proper 1.0.0 Release

--- data.lua
+++ data.lua
@@ -97,61 +97,6 @@ local llrRocketFuelRecipe = {
     result = "llr-rocket-fuel"
 }
 
-local SpoiledPlastic = {
-  type = "item",
-  name = "spoiled-plastic",
-  icon = "__AdvancedFuelProcessing__/graphics/Spoiled_plastic_bar.png",
-  icon_size = 64, icon_mipmaps = 4,
-  stack_size = 100
-}
-
-local HeavyOilPlasticRecipe = {
-  type = "recipe",
-  name = "alternative-plastic",
-  icon = "__AdvancedFuelProcessing__/graphics/Plastic_from_Heavy_Oil.png",
-  icon_size = 64, icon_mipmaps = 4,
-  energy_required = 0.5,
-  enabled = false,
-  category = "chemistry",
-  ingredients = {{type = "fluid", name = "petroleum-gas", amount = 10}, {type = "fluid", name = "heavy-oil", amount = 5}},
-  results = {
-    {type = item, name = "plastic-bar", amount = 1, probability = 0.9},
-    {type = item, name = "spoiled-plastic", amount = 2, probability = 0.1}
-  },
-  main_product = "plastic-bar"
-}
-
-local SpoiledPlasticSteamRecycling = {
-  type = "recipe",
-  name = "plastic-steam-recycling",
-  icon = "__AdvancedFuelProcessing__/graphics/Plastic_recycling_icon.png",
-  icon_size = 64, icon_mipmaps = 4,
-  energy_required = 5,
-  enabled = false,
-  category = "chemistry",
-  ingredients = {{type = "fluid", name = "steam", amount = 10}, {name = "spoiled-plastic", amount = 1}},
-  results = {
-    {type = item, name = "plastic-bar", amount = 1, probability = 0.5},
-    {type = item, name = "spoiled-plastic", amount = 1, probability = 0.5}
-  },
-  main_product = "plastic-bar"
-}
-local SpoiledPlasticAcidRecycling = {
-  type = "recipe",
-  name = "plastic-acid-recycling",
-  icon = "__AdvancedFuelProcessing__/graphics/Plastic_acid_recycling.png",
-  icon_size = 64, icon_mipmaps = 4,
-  energy_required = 5,
-  enabled = false,
-  category = "chemistry",
-  ingredients = {{type = "fluid", name = "sulfuric-acid", amount = 10}, {name = "spoiled-plastic", amount = 1}},
-  results = {
-    {type = item, name = "plastic-bar", amount = 1, probability = 0.3},
-    {type = item, name = "spoiled-plastic", amount = 1, probability = 0.7}
-  },
-  main_product = "plastic-bar"
-}
-
 local CoalExperimentationResearch = {
     type = "technology",
   name = "Coal-experimentation-research",
@@ -200,18 +145,10 @@ local SolidFuelExperimentationResearch = {
   },
 }
 
-
-
 table.insert(data.raw.technology["rocket-fuel"].effects,{type = "unlock-recipe",recipe = "llr-rocket-fuel"})
-table.insert(data.raw.technology["plastics"].effects,{type = "unlock-recipe",recipe = "alternative-plastic"})
-table.insert(data.raw.technology["plastics"].effects,{type = "unlock-recipe",recipe = "plastic-steam-recycling"})
-table.insert(data.raw.technology["plastics"].effects,{type = "unlock-recipe",recipe = "plastic-acid-recycling"})
-
-data.raw["recipe"]["plastic-bar"].results = {{type = item, name = "plastic-bar", amount = 2, probability = 0.9},{type = item, name = "spoiled-plastic", amount = 4, probability = 0.1}}
-data.raw["recipe"]["plastic-bar"].main_product = "plastic-bar"
 
 data.raw["car"]["car"].burner.fuel_categories = {"vehicle-fuel","chemical"}
 data.raw["car"]["tank"].burner.fuel_categories = {"vehicle-fuel","chemical"}
 data.raw["locomotive"]["locomotive"].burner.fuel_categories = {"vehicle-fuel","chemical"}
 
-data:extend{VehicleFuel, SolidHeavyOil, SolidHeavyOilRecipe, SolidLightOil, SolidLightOilRecipe, SolidCoal, SolidCoalRecipe, llrRocketFuel, llrRocketFuelRecipe, SpoiledPlastic, HeavyOilPlasticRecipe, SpoiledPlasticSteamRecycling, SpoiledPlasticAcidRecycling, CoalExperimentationResearch, SolidFuelExperimentationResearch}
+data:extend{VehicleFuel, SolidHeavyOil, SolidHeavyOilRecipe, SolidLightOil, SolidLightOilRecipe, SolidCoal, SolidCoalRecipe, llrRocketFuel, llrRocketFuelRecipe, CoalExperimentationResearch, SolidFuelExperimentationResearch}
